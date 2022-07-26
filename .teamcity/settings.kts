ac0ef3917f23764587c456b72229de37472b90ab
Paras <p.devakumaran@solarwinds.com>
2022-07-26 19:52:18 +0530

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,7 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.MavenBuildStep
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.kubernetesCloudProfile
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -31,46 +29,22 @@ version = "2022.04"
 
 project {
 
-    vcsRoot(HttpsGithubComMarcobehlerjetbrainsBuildpipelinesRefsHeadsMain)
+    vcsRoot(HttpsGithubComPdevakumaranSwiTeamtest2refsHeadsMaster)
 
     buildType(Build)
-
-    features {
-        kubernetesCloudProfile {
-            id = "kube-1"
-            name = "profile1"
-            description = "profile1"
-            terminateIdleMinutes = 30
-            apiServerURL = "localhost-kubernetes.com"
-            authStrategy = defaultServiceAccount()
-            param("kubeconfigContext", "")
-        }
-    }
 }
 
 object Build : BuildType({
     name = "Build"
 
     vcs {
-        root(HttpsGithubComMarcobehlerjetbrainsBuildpipelinesRefsHeadsMain)
+        root(HttpsGithubComPdevakumaranSwiTeamtest2refsHeadsMaster)
     }
 
     steps {
         maven {
             name = "maven-step-1"
             goals = "clean"
-            pomLocation = "calculator-service/pom.xml"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            dockerImagePlatform = MavenBuildStep.ImagePlatform.Windows
-            dockerImage = "maven:latest"
-        }
-        maven {
-            name = "maven-step-2"
-            enabled = false
-            goals = "clean"
-            pomLocation = "authorization-service/pom.xml"
-            dockerImagePlatform = MavenBuildStep.ImagePlatform.Linux
-            dockerPull = true
         }
     }
 
@@ -80,9 +54,9 @@ object Build : BuildType({
     }
 })
 
-object HttpsGithubComMarcobehlerjetbrainsBuildpipelinesRefsHeadsMain : GitVcsRoot({
-    name = "https://github.com/marcobehlerjetbrains/buildpipelines#refs/heads/main"
-    url = "https://github.com/marcobehlerjetbrains/buildpipelines"
-    branch = "refs/heads/main"
+object HttpsGithubComPdevakumaranSwiTeamtest2refsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/pdevakumaran-swi/teamtest2#refs/heads/master"
+    url = "https://github.com/pdevakumaran-swi/teamtest2"
+    branch = "refs/heads/master"
     branchSpec = "refs/heads/*"
 })
